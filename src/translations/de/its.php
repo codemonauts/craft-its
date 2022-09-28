dd267276d2c3b3167f7b6da4225713db9cadee37
Tom Schulze <tom@codemonauts.com>
2022-09-28 14:18:25 +0200

    add translations

---
+++ src/translations/de/its.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Are you sure you want to delete this issue?' => '',
+    'Add Issue' => '',
+    'Issue Types' => '',
+    'Name' => '',
+    'What this issue type will be called in the control panel.' => '',
+    'Handle' => '',
+    'How you’ll refer to this issue type in the templates and when a new issue is created programmatically.' => '',
+    'New issue type' => '',
+    'Issues' => '',
+    'Edit Issue Type' => '',
+    'Create a new issue type' => '',
+    'Couldn’t save issue type.' => '',
+    'Issue type saved.' => '',
+    'Issue' => '',
+    'issue' => '',
+    'issues' => '',
+    'All issues' => '',
+    'Subject' => '',
+    'The subject of the issue.' => '',
+];
